e8aaa0fdf68274df4f2eaa51f0fde98528fefb78
Marcos <marcosbozzani@gmail.com>
2018-05-14 18:11:52 -0300

    feat: add commands to change local and remote current directory

--- bin/index.js
+++ bin/index.js
@@ -6,59 +6,66 @@ const yaml = require('js-yaml')
 const Burguer = require('../lib')
 
 async function start() {
-    const script = loadScript()
-    const burguer = new Burguer(script.config)
+  const script = loadScript()
+  const burguer = new Burguer(script.config)
 
-    for (const command of script.commands) {
-        let name = ''
-        let args = []
-        if (typeof command === 'string') {
-            name = command
-        }
-        else {
-            name = Object.keys(command)[0]
-            args = command[name]
-            if (!Array.isArray(args)) {
-                args = [ args ]
-            }
-        } 
+  for (const command of script.commands) {
+    let name = ''
+    let args = []
+    if (typeof command === 'string') {
+      name = command
+    }
+    else {
+      name = Object.keys(command)[0]
+      args = command[name]
+      if (!Array.isArray(args)) {
+        args = [args]
+      }
+    }
 
-        if (name === 'local') {
-            await burguer.local(...args)
-        }
-        else if (name === 'remote') {
-            await burguer.remote(...args)
-        }
-        else if (name === 'push') {
-            await burguer.push(...args)
-        }
-        else if (name === 'pull') {
-            await burguer.pull(...args)
-        }
-        else if (name === 'connect') {
-            await burguer.connect()
-        }
-        else if (name === 'disconnect') {
-            burguer.disconnect()
-        }
-        else {
-            console.error(`command not found: ${name}`)
-        }
-    }    
+    if (name === 'local') {
+      await burguer.local(...args)
+    }
+    else if (name === 'local.cd') {
+      burguer.local.cd(...args)
+    }
+    else if (name === 'remote') {
+      await burguer.remote(...args)
+    }
+    else if (name === 'remote.cd') {
+      burguer.remote.cd(...args)
+    }
+    else if (name === 'push') {
+      await burguer.push(...args)
+    }
+    else if (name === 'pull') {
+      await burguer.pull(...args)
+    }
+    else if (name === 'connect') {
+      await burguer.connect()
+    }
+    else if (name === 'disconnect') {
+      burguer.disconnect()
+    }
+    else {
+      console.error(`command not found: ${name}`)
+      process.exit(1)
+    }
+  }
 }
 
 function loadScript() {
-    const script = path.resolve(process.argv[2])
-    if (!fs.existsSync(script)) {
-        console.error(`file not found: ${script}`)
-        process.exit(-1)    
-    }
-    return yaml.safeLoad(fs.readFileSync(script, 'utf8'))
+  const script = path.resolve(process.argv[2])
+  if (!fs.existsSync(script)) {
+    console.error(`file not found: ${script}`)
+    process.exit(-1)
+  }
+  return yaml.safeLoad(fs.readFileSync(script, 'utf8'))
 }
 
 function errorHandler(error) {
-    console.error(error)
-    process.exit(-1)
+  console.error(error)
+  process.exit(-1)
 }
 
 process.on('uncaughtException', errorHandler)
